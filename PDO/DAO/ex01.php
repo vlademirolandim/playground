a5d9e47ee490b2483bce781d92a687a407bf249f
vlademirolandim <vlademirolandim@gmail.com>
2022-08-03 14:47:15 +0000

    Alteração na Classe DAO para testar o arquivo na memória

--- PDO/DAO/ex01.php
+++ PDO/DAO/ex01.php
@@ -3,14 +3,8 @@ class Sql{
     private $conn;
 
     public function __construct(){
-        $this->conn=new PDO('sqlite:' . __DIR__ . DIRECTORY_SEPARATOR .  'clientes.sqlite');
-        $sql="CREATE TABLE IF NOT EXISTS clientes (
-            ID_CLIENTE INTEGER PRIMARY KEY AUTOINCREMENT,
-            NOME VARCHAR(255) NOT NULL );
-        ";
-    
-        $stmt=$this->conn->prepare($sql);
-        $stmt->execute();
+        $this->conn=new PDO('sqlite::memory:'); // __DIR__ . DIRECTORY_SEPARATOR .  'clientes.sqlite');
+        $this->criaDados();
     }
 
 
@@ -27,9 +21,25 @@ class Sql{
         $stmt=$this->query($sql,$params );
         return $stmt->fetchAll( PDO::FETCH_ASSOC );
     }
+
+    public function criaDados(){
+    
+        $sql="CREATE TABLE IF NOT EXISTS clientes (
+            ID_CLIENTE INTEGER PRIMARY KEY AUTOINCREMENT,
+            NOME VARCHAR(255) NOT NULL );
+        ";
+    
+        $stmt=$this->conn->prepare($sql);
+        $stmt->execute();
+    
+    }
+    
 }
 
-echo __DIR__;
+
+/**
+ * Inicializando a classe e fazendo os testes
+ */
 $usu = new Sql();
 $usu->query( "INSERT INTO clientes ( NOME ) VALUES ( :NOME )", [ "NOME" => "Dantasy" ] );
 var_dump( $usu->select( "select * from clientes") );
