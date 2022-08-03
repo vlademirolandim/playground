153db2c537c74a3a43d79f0c187240b6b9a44850
vlademirolandim <vlademirolandim@gmail.com>
2022-08-03 15:03:31 +0000

    rename DAO folder to DAO00

---
+++ PDO/DAO01/ex01.php
@@ -0,0 +1,45 @@
+<?php
+class Sql{
+    private $conn;
+
+    public function __construct(){
+        $this->conn=new PDO('sqlite::memory:'); // __DIR__ . DIRECTORY_SEPARATOR .  'clientes.sqlite');
+        $this->criaDados();
+    }
+
+
+    public function query($sql, $params=[]) {
+        $stmt = $this->conn->prepare($sql);
+        foreach ($params as $key => $value){
+            $stmt->bindParam( $key, $value );
+        }
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function select( $sql, $params=[] ){
+        $stmt=$this->query($sql,$params );
+        return $stmt->fetchAll( PDO::FETCH_ASSOC );
+    }
+
+    public function criaDados(){
+    
+        $sql="CREATE TABLE IF NOT EXISTS clientes (
+            ID_CLIENTE INTEGER PRIMARY KEY AUTOINCREMENT,
+            NOME VARCHAR(255) NOT NULL );
+        ";
+    
+        $stmt=$this->conn->prepare($sql);
+        $stmt->execute();
+    
+    }
+    
+}
+
+
+/**
+ * Inicializando a classe e fazendo os testes
+ */
+$usu = new Sql();
+$usu->query( "INSERT INTO clientes ( NOME ) VALUES ( :NOME )", [ "NOME" => "Dantasy" ] );
+var_dump( $usu->select( "select * from clientes") );
